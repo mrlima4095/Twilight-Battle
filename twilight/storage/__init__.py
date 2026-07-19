37acd57d032142f62089b80b71f8b1908d175ee1
Mr Lima <felipebr4095@gmail.com>
2026-07-18 22:20:42 -0300

    twilight.storage.db

--- twilight/storage/__init__.py
+++ twilight/storage/__init__.py
@@ -1,13 +1,17 @@
 """Persistência em disco (data/)."""
 
 from twilight.storage.admin_levels import load_admin_levels, save_admin_levels
+from twilight.storage.db import init_db, load_accounts, save_accounts
 from twilight.storage.journal import load_journal, save_journal
 from twilight.storage.story_saves import get_user_save_file
 
 __all__ = [
     'get_user_save_file',
+    'init_db',
+    'load_accounts',
     'load_admin_levels',
     'load_journal',
+    'save_accounts',
     'save_admin_levels',
     'save_journal',
 ]
